e983edf94f5ce98db91a321148c70f85fa391890
Rando2020 <75230570+Rando2020@users.noreply.github.com>
2026-07-17 15:19:01 -0500

    feat: initialize learner authentication context

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { AuthProvider } from './context/AuthContext'
 import './styles1.css'
 import './styles2.css'
 import './styles3.css'
 import './styles4.css'
+import './styles-lms.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </StrictMode>,
 )
